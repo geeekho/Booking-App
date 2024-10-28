43519d3e1e6699950894614f522539cf87ba00a0
geeekho <wajdi.sadouki@gmail.com>
2024-10-28 15:34:56 +0100

    prepared the sidebar content placeholders

---
+++ src/components/Devbar/Intro.jsx
@@ -0,0 +1,39 @@
+import { Separator } from "../ui";
+
+export const Intro = () => {
+  return (
+    <div>
+      <h2>Welcome to Project React!</h2>
+      <Separator className="mb-2" />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium
+        autem fuga pariatur quidem rerum id nulla quasi minus. Cum deserunt est
+        ipsum laborum ut quaerat quam molestias aliquid? Eum, culpa!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+        consequatur, modi eveniet inventore aliquam nesciunt minima temporibus
+        dolores labore earum, provident sit molestias sint distinctio a adipisci
+        atque quas quidem?
+      </p>
+      <h3>Description</h3>
+      <Separator className="mb-2" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ex,
+        saepe, excepturi accusamus dolorem sequi enim laboriosam, veniam ipsum
+        eum incidunt! Tempore odio a cupiditate. Voluptatum maiores inventore
+        veritatis a.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+        necessitatibus cupiditate sunt deleniti quam ipsa quo ullam expedita
+        nobis quasi, asperiores id modi dolorem consequatur excepturi quas iure
+        possimus nesciunt?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae amet
+        debitis dignissimos.
+      </p>
+    </div>
+  );
+};
